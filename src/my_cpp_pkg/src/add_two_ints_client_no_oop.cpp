bd3c53ee74ad2553bf2de01ed03cfff26e639e2c
Rajana Kavinda Samarakoon <rajana.k.samarakoon@gmail.com>
2023-09-06 12:28:51 +0530

    add_two_ints_no_oop.cpp done.

--- src/my_cpp_pkg/src/add_two_ints_client_no_oop.cpp
+++ src/my_cpp_pkg/src/add_two_ints_client_no_oop.cpp
@@ -7,6 +7,24 @@ int main(int argc, char **argv)
     auto node = std::make_shared<rclcpp::Node>("add_two_ints_client_no_oop");
     
     auto client = node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+    while (!client->wait_for_service(std::chrono::seconds(1))) 
+    {
+        RCLCPP_WARN(node->get_logger(), "Waiting for the server to be up...");
+    }
+
+    auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+    request->a = 3;
+    request->b = 8;
+
+    auto future = client->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(node, future) == rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_INFO(node->get_logger(), "Result of %ld + %ld = %ld", request->a, request->b, future.get()->sum);
+    }
+    else
+    {
+        RCLCPP_ERROR(node->get_logger(), "Failed to call service add_two_ints");
+    }
 
     rclcpp::shutdown();
     return 0;
